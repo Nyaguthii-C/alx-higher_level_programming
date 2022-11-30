ce753cce8e5738f7d98a7b7c62d5a878cd5cc8ef
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-11-29 17:28:43 -0800

    add error for trial in declaring last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_number = int(repr(number)[-1])
+last_number = number[-1]
 if number < 0:
     last_number *= -1
     str = "and is less than 6 and not 0"
@@ -11,4 +11,4 @@ elif last_number > 5:
     str = "and is greater than 5"
 else:
     str = "and is 0"
-print(f"Last digit of {number} is {last_number} {str}")
+print(f"Last digit of {number:d} is {last_number:d} {str}")
