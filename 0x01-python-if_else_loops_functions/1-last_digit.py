7087b5cf961d27ca8030bef042c9715a6df00310
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-11-29 17:33:13 -0800

    use modulo operator to get last digit of number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_number = number[-1]
+last_number = number % 10
 if number < 0:
     last_number *= -1
     str = "and is less than 6 and not 0"
