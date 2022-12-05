6904f5b717f3745b9d3bb0400141ef71440fc0e6
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-12-04 17:28:37 -0800

    implement pycodestyle

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-for i in range (1, 101):
+for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
-        print(f"FizzBuzz", end = " ")
+        print(f"FizzBuzz", end=" ")
     elif i % 3 == 0:
-        print(f"Fizz", end = " ")
+        print(f"Fizz", end=" ")
     elif i % 5 == 0:
-        print(f"Buzz", end = " ")
+        print(f"Buzz", end=" ")
     else:
-        print(f"{i:d}", end = " ")
+        print(f"{i:d}", end=" ")
