c9d0efec44fdde39e86699f99436cb0b54fe8765
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-12-04 18:03:41 -0800

    define fizzbuzz but do not call function

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def fizzbuzz():
     for fizzbuzz in range(1, 101):
         if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
@@ -10,6 +9,3 @@ def fizzbuzz():
             print(f"Buzz", end=" ")
         else:
             print("{}".format(fizzbuzz), end=" ")
-
-
-fizzbuzz()
