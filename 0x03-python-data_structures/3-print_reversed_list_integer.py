99e5cba5191b46382a94f690170612d216e06d05
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-12-06 17:53:06 -0800

    add edge case if data passed not list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
+    if type(my_list) == list:
+        my_list.reverse()
+    else:
+        return
     for i in my_list:
         print("{:d}".format(i))
