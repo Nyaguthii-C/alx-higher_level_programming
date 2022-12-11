260b5fa00fbcdf2c63e1205c15a057aa4fb6be05
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-12-10 16:39:18 -0800

    remove string format, give return value

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def pow(a, b):
-    pow = a**b
-    print("{:d}".format(pow))
+    return (a**b)
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
