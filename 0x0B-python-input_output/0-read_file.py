83db3d0efff9d32cc8e6fb4f51e7ab3b76d29d02
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-01-11 15:52:38 -0800

    implement pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+""" reads a text file (UTF8) and prints it to stdout """
+
+
 def read_file(filename=""):
-    """
-    reads a text file (UTF8) and prints it to stdout
-    """
+    """ opens the file encoded with utf-8 and prints content"""
     with open(filename, encoding="utf-8") as f:
-        print(f.read(), end = "")
+        print(f.read(), end="")
