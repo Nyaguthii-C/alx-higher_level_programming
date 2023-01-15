1e48e401cea2472e8f5bc9ff3678f355e48f2304
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-01-14 20:16:34 -0800

    implement pycodestyle

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ return string without c and C """
+
+
 def no_c(my_string):
     new_string = ''
-    for i in  my_string:
-        if i != 'c' and i !='C':
-           new_string += i
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_string += i
     return(new_string)
