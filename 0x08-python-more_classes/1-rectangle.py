062009d584c28681610dac517f4ddba5e01a1d51
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-01-16 18:44:27 -0800

    edit height attribute

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,26 +16,28 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
+    @property
     def width(self):
-        self.__width = width
+        return self.__width
 
     def width(self, value):
         if width >= 0 and width <= 9:
             def set_width(self, width):
-                self.__width = width
+                self.__width = value
         elif width < 0:
             raise ValueError('width must be >= 0')
         else:
             raise TypeError('width must be an integer')
 
+    @property
     def height(self):
-        self.__height = height
+        return self.__height
 
     def height(self, value):
-        if width >= 0 and width <= 9:
-            def set_width(self, width):
-                self.__width = width
-        elif width < 0:
-            raise ValueError('width must be >= 0')
+        if height >= 0 and height <= 9:
+            def set_height(self, height):
+                self.__height = value
+        elif height < 0:
+            raise ValueError('height must be >= 0')
         else:
-            raise TypeError('width must be an integer')
+            raise TypeError('height must be an integer')
