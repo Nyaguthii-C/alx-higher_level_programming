9c2c3fe00701a231149a8e264393bab3e15f1690
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-01-16 20:04:30 -0800

    add private instance attr to square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A class Square with private instance attribute size """
+
+
+class Square:
+    def __init__(self, size):
+        """ define a the size of the square """
+        self.__size = size
