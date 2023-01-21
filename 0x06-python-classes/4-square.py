09090b8d5264a933284be68ae4c82244a5e0f897
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-01-20 19:48:07 -0800

    add property setter for attribute size

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+A class Square with private instance attribute size
+and a public instance method area
+and property setter for private attribut size
+"""
+
+
+class Square:
+    """ define a the size of the square """
+    def __init__(self, size=0):
+        """ Instantiation of size """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """retrieves size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the value of size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ returns the current square area """
+        return (self.__size ** 2)
