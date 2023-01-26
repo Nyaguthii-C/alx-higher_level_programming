84dd945b2462b76e1d2f5826ef4a3126a262c86c
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-01-25 20:51:28 -0800

    add documentation

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     """
-
+    Args:
+     - a, b - integers to be divided
+    returns:
+        value after division
     """
     try:
         x = a/b
     except (ZeroDivisionError, ValueError):
         x = None
     finally:
-        print("Inside result:{}".format(x))
+        print("Inside result:{}".format(x) end="")
     return x
