711264fa854a5d744088308632523259b0124e2f
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-01-25 19:00:10 -0800

    print x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+prints x elements of a list
+"""
+
+
+def safe_print_list(my_list=[], x=0):
+    """
+    Args:
+        my_list[] - list of elements to be printed
+        x - number of elements to be printed
+    Returns:
+        real number of elements printed
+    """
+    elems = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end=" ")
+            elems += 1
+        except IndexError:
+            break
+    print(" ")
+    return elems
