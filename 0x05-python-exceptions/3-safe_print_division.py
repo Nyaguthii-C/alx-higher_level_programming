781dcce202ce0311a1646fff079d27ae8de58bd2
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-01-25 20:53:06 -0800

    script update

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -11,5 +11,5 @@ def safe_print_division(a, b):
     except (ZeroDivisionError, ValueError):
         x = None
     finally:
-        print("Inside result:{}".format(x) end="")
+        print("Inside result:{}".format(x))
     return x
