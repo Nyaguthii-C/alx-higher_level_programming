06bdf2bda312c1fd19ee174548516beacbb94de9
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-02-03 18:21:02 -0800

    return the dictionary description with simple data structure

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -7,4 +7,4 @@ def class_to_json(obj):
         Args - obj. object will be any dat type: class, dict, list...
         Returns - dict of object
     """
-    return 0bj.__dict__
+    return obj.__dict__
