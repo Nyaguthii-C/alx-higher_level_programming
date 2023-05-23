d6e20df5f2b6d695a312127cf0cf23008f22b434
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-05-23 13:41:07 -0700

    add  __name__ == "__main__" to script

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from variable_load_5 import a
 """
 a program that imports the variable a from the file variable_load_5.py
 and prints its value
 """
 
 
-print(a)
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print(a)
