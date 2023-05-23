63c57cd02aec14f5181aaf1c42a0e25256a57c1b
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-05-23 13:43:31 -0700

    import a variable from file print value

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -2,6 +2,8 @@
 """
 a program that imports the variable a from the file variable_load_5.py
 and prints its value
+ __name__ == "__main__" Allows You to
+ Execute Code When the File Runs as a Script
 """
 
 
