01abec7f691c978944257e6e9f036d966de8a5b1
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-05-23 22:21:57 -0700

    implement pycodestyle

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,17 +7,16 @@
 def add_integer(a, b=98):
     """
     Args:
-        a: integer or to be added 
-	b: integer or to be added 
+        a: integer or to be added
+        b: integer or to be added
     Returns:
         an integer: the addition of a and b
-	or raises TypeError if args not int or float
+        or raises TypeError if args not int or float
     """
-    if not isinstance(a, int) and not isinstance (a, float):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance (b, float):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError('b must be an integer')
-    
     a = int(a)
     b = int(b)
     return (a+b)
