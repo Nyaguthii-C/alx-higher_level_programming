9276337d87d490582171938a6dec6f702d014d7c
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-05-30 19:18:54 -0700

    implement pycodestyle

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ a function that returns a key with the biggest integer value
 
 
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        return max(a_dictionary, key = a_dictionary.get)
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
     else:
         return None
