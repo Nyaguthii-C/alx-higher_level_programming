d34a91c20e487b735f6b2aa4cdb028b0bfa8e71d
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-06-14 16:44:31 -0700

    print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,13 +4,16 @@
 const args = process.argv;
 const size = args[2];
 let Xter = '';
+/* define limits for length and width */
 for (let i = 0; i < size; i++) {
   for (let j = 0; j < size; j++) {
     Xter += 'X';
   }
-  Xter += '\n';
+  /* print the square after iterating */
+  console.log(Xter);
+  /* reset character to empty after printing */
+  Xter = '';
 }
 if (isNaN(size)) {
   console.log('Missing size');
 }
-console.log(Xter);
