be415110c3a14159501b1ed58ef7950f157ec0a2
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-06-14 12:27:42 -0700

    print strings in array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+/* prints 3 lines using an array of string and a loop */
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; myArray[i]; i++) {
+  console.log(myArray[i]);
+}
