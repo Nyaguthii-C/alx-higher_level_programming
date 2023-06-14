3c9ce10ee9d07797d6376dfc40d6bfc66f7398cf
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-06-13 18:31:06 -0700

    print 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+/* a script that prints 3 lines */
+const cProg = 'C is fun';
+const pyProg = 'Python is cool';
+const jS = 'JavaScript is amazing';
+console.log(cProg); console.log(pyProg); console.log(jS);
