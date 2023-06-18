bd26ec9559ebacba1a3e98a52713850da54f36d3
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-06-18 11:18:53 -0700

    create instance method to print square using c

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/**
+* Create an instance method called charPrint(c)
+* that prints the rectangle using the character c
+* If c is undefined, use the character X
+*/
+module.exports = class Square extends require('./4-rectangle.js') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let XterC = '';
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          XterC += 'c';
+        }
+        console.log(XterC);
+        XterC = '';
+      }
+    }
+  }
+};
