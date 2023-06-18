7b1d269c809d4a34dd996bb8ab545065b27621ef
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-06-18 16:45:06 -0700

    import constructor from 5-square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,22 +4,13 @@
 * that prints the rectangle using the character c
 * If c is undefined, use the character X
 */
-module.exports = class Square extends require('./4-rectangle.js') {
-  constructor (size) {
-    super(size, size);
-  }
-
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
-    let XterC = '';
     if (c === undefined) {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) {
-          XterC += 'C';
-        }
-        console.log(XterC);
-        XterC = '';
+        console.log(c.repeat(this.width));
       }
     }
   }
