79a47034726372667ec6f4bf6600283ccf7cbeba
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-07-05 17:05:49 -0700

    make it short

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 #sends a request to a URL passed as an argument, and displays only the status code of the response
-
-response=$(curl -s -X POST 0.0.0.0:5000/catch_me)
-
-if [[ $response == *"You got me!"* ]]; then
-  printf "%s\n" "Server response: $response"
-else
-  printf "%s\n" "Failed to capture server response."
-fi
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
