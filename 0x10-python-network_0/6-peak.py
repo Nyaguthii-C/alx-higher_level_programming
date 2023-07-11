1cb94f6c707a1c58b32fcf025fd45327a6732999
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-07-10 18:54:24 -0700

    find peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
-""" a function that finds a peak in a list of unsorted integers"""
+"""
+a function that finds a peak in a list of unsorted integers
+without examining all the elements
+"""
 
 
 def find_peak(list_of_integers):
-    for (int i=0, i < size(list_of_integers); i++)
-        for (int j=1, j <= size(list_of_integers); j++)
-            if (list_of_integers[i] < list_of_integers[j])
-                continue
+    """
+    function: finds peak in list via binary search
+    args: list_of_integers
+    """
+    n = len(list_of_integers)
+    if n == 0:
+        return None
+    if n == 1:
+        return list_of_integers[0]
+    if n == 2:
+        return max(list_of_integers)
+    elif list_of_integers[0] > list_of_integers[1]:
+        return list_of_integers[0]
+    elif list_of_integers[n - 1] >= list_of_integers[n - 2]:
+        return list_of_integers[n - 1]
+    else:
+        left = 1
+        right = n - 2
+        while left <= right:
+            mid = (left + right) // 2
+            if list_of_integers[mid] >= list_of_integers[mid - 1] and \
+                    list_of_integers[mid] >= list_of_integers[mid + 1]:
+                return list_of_integers[mid]
+            elif list_of_integers[mid] < list_of_integers[mid - 1]:
+                right = mid - 1
             else:
-                print(f'{i}')
+                left = mid + 1
