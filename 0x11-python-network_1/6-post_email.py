43d6d69c4f7d4b3e08ed2643a1b05e4e16f725fb
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-07-13 18:57:45 -0700

    post an email using requests package

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,7 +11,6 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode('ascii')
 
     req = requests.post(url, data=value)
     print(req.text)
