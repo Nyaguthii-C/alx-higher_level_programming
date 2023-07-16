64f80dc9e30db8ac4decb28d20234e1d430c9c71
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-07-16 13:58:21 -0700

    list the last 10 commits in a given repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+takes 2 arguments list 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”
+Usage: ./100-github_commits.py rails rails
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits"\
+        .format(sys.argv[1], sys.argv[2])
+    req = requests.get(url)
+    req = req.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                req[i].get("sha"),
+                req[i].get("commit").get("author").get("name")))
+    except IndexError as e:
+        pass
