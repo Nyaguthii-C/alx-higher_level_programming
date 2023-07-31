18153907b5594cabc88d1cfae684645c0eaa94fc
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-07-31 02:00:04 +0000

    make semistandard compliant

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-//reads and prints the content of a file
-//where the content of the file must be read in utf-8
-
+// reads and prints the content of a file
+// where the content of the file must be read in utf-8
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function(error, readData) {
+fs.readFile(process.argv[2], 'utf8', function (error, readData) {
   console.log(error || readData);
 });
-
