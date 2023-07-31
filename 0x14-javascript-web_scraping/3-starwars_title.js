1a0dae1205e6aff7c37c4ccc9df9fb22becab8f5
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-07-31 04:19:06 +0000

    print movie title given episode number from an API

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// prints title of a Star Wars movie where episode number matches a given integer
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const episodeNum = process.argv[2];
+
+request(url + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const response = JSON.parse(body);
+    console.log(response.title);
+  } else {
+    console.log('Error ' + response.statusCode);
+  }
+});
