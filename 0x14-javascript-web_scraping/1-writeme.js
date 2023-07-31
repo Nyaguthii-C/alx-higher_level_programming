f2391a09ae20f6025d4e83edaf3cfa3b6013cc94
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-07-31 02:01:11 +0000

    make semistandard compliant

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-//writes a string to a file
+// writes a string to a file
 
 const fs = require('fs');
-const data = process.argv[3]
+const data = process.argv[3];
 
-fs.writeFile(process.argv[2], data , error => {
+fs.writeFile(process.argv[2], data, error => {
   if (error)console.log(error);
 });
-
