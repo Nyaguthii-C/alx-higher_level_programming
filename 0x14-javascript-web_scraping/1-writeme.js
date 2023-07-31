3054934210e2c75cf8163476b36da21383afecc2
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-07-31 01:56:51 +0000

    declare variables using const intead of var

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 //writes a string to a file
 
-var fs = require('fs');
-var data = process.argv[3]
+const fs = require('fs');
+const data = process.argv[3]
 
 fs.writeFile(process.argv[2], data , error => {
   if (error)console.log(error);
