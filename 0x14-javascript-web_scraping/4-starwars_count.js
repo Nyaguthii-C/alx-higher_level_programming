9252fc8bdc8029b3506e3dd6234d67a2a44b073c
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-07-31 19:32:17 +0000

    print the number of movies where the Wedge is present

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// prints title of a Star Wars movie where episode number matches a given integer
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const results = JSON.parse(body).results;// get list of movies
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1// if condition true return increment
+        : count;// else return initial vslue
+    }, 0));// 0 is the initial vlue of count
+  }
+});
