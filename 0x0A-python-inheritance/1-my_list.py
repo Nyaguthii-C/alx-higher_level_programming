9d6ccc1663cb0cda572aaf18892a36cbd76f56b4
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-08-23 18:53:13 -0700

    write a class that inherits a list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a function that sorts and prints a list"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        """sort and print inherited list"""
+        print(sorted(self))
